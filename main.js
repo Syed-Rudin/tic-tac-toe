77fea9d2eb974b94a2fba954c358897fc827a5a1
Syed-Rudin <syedrudin04@gmail.com>
2021-08-13 15:15:52 +0800

    Created gameboard display from JS module

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const gameBoard = (() => {
-    
+    function createBoard() {
+        let board = document.createElement('div');
+        board.classList.add('gameboard');
+        for (let i = 0; i < 9; i++) {
+            let square = document.createElement('div');
+            square.classList.add('gameitem');
+            board.appendChild(square);
+        }
+
+        document.body.appendChild(board);
+    }
+
+    return {
+        createBoard
+    }
 })();
+
+gameBoard.createBoard();
