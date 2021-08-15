c1c06875b33e3dce37f90b2acb857f9219005edf
Syed-Rudin <syedrudin04@gmail.com>
2021-08-15 20:25:28 +0800

    Added check for winner functionality and restart button. Restart button not styled yet. No known bugs.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const gameBoard = (() => {
     let turn = 'X';
     let squares = document.getElementsByClassName('gameitem');
+    let display = document.querySelector('.display');
+    display.textContent = 'It\'s player X\'s turn';
 
     // Create board and individual squares
     function createBoard() {
@@ -20,13 +22,14 @@ const gameBoard = (() => {
     function _changeTurn() {
         if (turn === 'X') {
             turn = 'O';
+            display.textContent = 'It\'s player O\'s turn';
             return 'X';
         } else if (turn === 'O') {
             turn = 'X';
+            display.textContent = 'It\'s player X\'s turn';
             return 'O';
         }
     };
-
 
     // Allow players to click squares to show 
     // either 'O' or 'X' depending on the turn
@@ -35,13 +38,14 @@ const gameBoard = (() => {
             let currentSquare = squares[i];
             currentSquare.addEventListener('click', function inputPlay() {
                 currentSquare.textContent = _changeTurn();
-                checkWinner();
+                resetGame();
                 currentSquare.removeEventListener('click', inputPlay);
             })
         }
     };
 
     // Check for a winner
+    // BUG: player O doesn't win even when it should in some cases
     function checkWinner() {
         if (squares[0].textContent === 'X') {
             if (squares[4].textContent === 'X' && squares[8].textContent === 'X') {
@@ -51,13 +55,15 @@ const gameBoard = (() => {
             } else if (squares[3].textContent === 'X' && squares[6].textContent === 'X') {
                 return 'X';
             }
-        } else if (squares[8].textContent === 'X') {
+        }
+        if (squares[8].textContent === 'X') {
             if (squares[2].textContent === 'X' && squares[5].textContent === 'X') {
                 return 'X';
             } else if (squares[6].textContent === 'X' && squares[7].textContent === 'X') {
                 return 'X';
             }
-        } else if (squares[4].textContent === 'X') {
+        }
+        if (squares[4].textContent === 'X') {
             if (squares[1].textContent === 'X' && squares[7].textContent === 'X') {
                 return 'X';
             } else if (squares[3].textContent === 'X' && squares[5].textContent === 'X') {
@@ -65,7 +71,8 @@ const gameBoard = (() => {
             } else if (squares[2].textContent === 'X' && squares[6].textContent === 'X') {
                 return 'X';
             }
-        } else if (squares[0].textContent === 'O') {
+        }
+        if (squares[0].textContent === 'O') {
             if (squares[4].textContent === 'O' && squares[8].textContent === 'O') {
                 return 'O';
             } else if (squares[1].textContent === 'O' && squares[3].textContent === 'O') {
@@ -73,13 +80,15 @@ const gameBoard = (() => {
             } else if (squares[3].textContent === 'O' && squares[6].textContent === 'O') {
                 return 'O';
             }
-        } else if (squares[8].textContent === 'O') {
+        }
+        if (squares[8].textContent === 'O') {
             if (squares[2].textContent === 'O' && squares[5].textContent === 'O') {
                 return 'O';
             } else if (squares[6].textContent === 'O' && squares[7].textContent === 'O') {
                 return 'O';
-            } 
-        } else if (squares[4].textContent === 'O') {
+            }
+        } 
+        if (squares[4].textContent === 'O') {
             if (squares[1].textContent === 'O' && squares[7].textContent === 'O') {
                 return 'O';
             } else if (squares[3].textContent === 'O' && squares[5].textContent === 'O') {
@@ -88,6 +97,40 @@ const gameBoard = (() => {
                 return 'O';
             }
         }
+    };
+
+    // Stop game and reset if players choose so
+    function resetGame() {
+        let result = checkWinner();
+        if (result === 'X') {
+            display.textContent = 'Player X wins!';
+            clearEverything();
+            
+        } else if (result === 'O') {
+            display.textContent = 'Player O wins!';
+            clearEverything();
+        }
+    };
+
+    // Clear the board 
+    function clearEverything() {
+        let restartBtn = document.createElement('button');
+        let board = document.querySelector('.gameboard');
+        restartBtn.textContent = "Restart"
+        restartBtn.addEventListener('click', function() {
+            for (let i = 0; i < 9; i++) {
+                let square = squares[i];
+                square.textContent = '';
+            }
+            board.remove();
+            restartBtn.remove()
+            gameBoard.createBoard();
+            gameBoard.playInSquare();
+            display.textContent = 'It\'s player X\'s turn';
+            turn = 'X';
+        })
+
+        document.body.appendChild(restartBtn);
     }
 
     return {
